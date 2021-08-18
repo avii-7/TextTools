e4ce5e5bfd91447c1e3ff444de51428cfb41333e
Avii <avii.official00@gmail.com>
2021-08-19 00:05:47 +0530

    New Componenets

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,23 +5,27 @@ export default function TextForm(props) {
   // this will not be destroyed ...current value can be used against another function call
   // use state will return an array with two item
   // [count,setcount] is state variables
-  const [count, setCount] = useState("Enter text here....");
+  const [Text, setText] = useState("");
   return (
+      <>
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" id="mybox" rows="8" placeholder="Enter text here" value={count}
-         onChange={(e) => {setCount(e.target.value)}}></textarea>
+        <textarea className="form-control" id="mybox" rows="8" placeholder="Enter text here" value={Text}
+         onChange={(e) => {setText(e.target.value)}}></textarea>
       </div>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          setCount(count.toUpperCase());
-        }}
-      >
-        UpperCase
-      </button>
+      <button className="btn btn-primary" onClick={() => {setText(Text.toUpperCase());}} > UpperCase </button>
+        <button className="btn btn-primary mx-3" onClick={() => {setText(Text.toLowerCase());}} > LowerCase </button>
     </div>
+    <div className="text-details my-3">
+        <h2>Text Details</h2>
+        <p>There are {Text.split(" ").length} words and {Text.length} characters.</p>
+    </div>
+    <div className="text-preview my-3">
+        <h2>Preview</h2>
+        <p> {Text} </p>
+    </div>
+    </>
   );
 }
 
